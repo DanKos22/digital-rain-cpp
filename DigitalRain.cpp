6f8bae4534a65b64188a3e334feb57053e07f616
DanKos22 <114909115+DanKos22@users.noreply.github.com>
2025-06-07 14:12:45 +0300

    Update DigitalRain.cpp

--- DigitalRain.cpp
+++ DigitalRain.cpp
@@ -11,19 +11,19 @@ Dan Koskiranta
 DigitalRain.cpp file defines the DigitalRain class, implementing its constructors and member functions
 */
 
-#include <iostream>			//cout
+#include <iostream>		//cout
 #include <windows.h>		//SetConsoleCursorPosition
 #include "DigitalRain.h"	//include DigitalRain class
-#include <vector>			//Collection of things of the same type. vector to stores positions
-#include <thread>			//control speed
-#include <chrono>			//time delay
+#include <vector>		//Collection of things of the same type. vector to stores positions
+#include <thread>		//control speed
+#include <chrono>		//time delay
 
 
-DigitalRain::DigitalRain() {}			//Initialize default constructor
+DigitalRain::DigitalRain() {}			                                //Initialize default constructor
 
 DigitalRain::DigitalRain(int color) : color_(color) {										
 	HANDLE theConsole = GetStdHandle(STD_OUTPUT_HANDLE);			//Get console output handle. Used for manipulating console's properties like text and cursor position
-	SetConsoleTextAttribute(theConsole, color);			            //Set the text color to green once in the constructor instead of setting the color repeatedly in the GotoXY function
+	SetConsoleTextAttribute(theConsole, color);			        //Set the text color to green once in the constructor instead of setting the color repeatedly in the GotoXY function
 }			
 
 
@@ -32,20 +32,20 @@ void DigitalRain::GotoXY(int x, int y) {
 	COORD coord;
 	coord.X = x;
 	coord.Y = y;
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);		//STD_OUTPUT_HANDLE refers to the console screen
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);	//STD_OUTPUT_HANDLE refers to the console screen
 }
 
 
 //Draw the falling characters
 void DigitalRain::DrawCharacters() {
 
-	int numberOfChars = 10;										//Number of falling characters
+	int numberOfChars = 10;						//Number of falling characters
 
-	std::vector<int> xPositions(numberOfChars);					//Store columnposition for each character
+	std::vector<int> xPositions(numberOfChars);			//Store columnposition for each character
 	std::vector<int> yPositions(numberOfChars);
-	std::vector<char> characters(numberOfChars);				//Vector to store the falling characters
-	std::vector<int> speeds(numberOfChars);						//Store different falling speeds
-	std::vector<int> timeCounters(numberOfChars, 0);			//Time counters for each character to track when the character should move down
+	std::vector<char> characters(numberOfChars);			//Vector to store the falling characters
+	std::vector<int> speeds(numberOfChars);				//Store different falling speeds
+	std::vector<int> timeCounters(numberOfChars, 0);		//Time counters for each character to track when the character should move down
 	std::vector<bool> charIsAtBottom(numberOfChars, false);		//Track if each character is at bottom
 
 	//Initialize random starting positions and characters.          Seeds the random number generator to get different random values on every run 
@@ -53,10 +53,10 @@ void DigitalRain::DrawCharacters() {
 
 	//Set the initial positions for characters
 	for (int i = 0; i < numberOfChars; ++i) {
-		xPositions[i] = 5 + (5 * i);								//Horizontal position for each character (5, 10, 15)
-		yPositions[i] = 0;											//Characters will start to fall from the top
+		xPositions[i] = 5 + (5 * i);				    //Horizontal position for each character (5, 10, 15)
+		yPositions[i] = 0;					    //Characters will start to fall from the top
 		characters[i] = static_cast<char>(33 + rand() % 10);        //(rand() generates a random integer value from ASCII 33 to 42. static_cast<char> is a type cast that converts the integer result into a character
-		speeds[i] = 2 + rand() % 5;									//Random falling speed between 2 and 6. speeds[i] controls how fast the character falls
+		speeds[i] = 2 + rand() % 5;				    //Random falling speed between 2 and 6. speeds[i] controls how fast the character falls
 	}
 
 	//Infinite loop to keep chracters falling
@@ -83,14 +83,14 @@ void DigitalRain::DrawCharacters() {
 				//if timecounters for a character reaches the speed threshold (speeds[i]), the character is allowed to move down
 				//controls how fast each character moves down by ensuring that characters with different speeds fall at different rates
 				if (++timeCounters[i] >= speeds[i]) {
-					timeCounters[i] = 0;								//Restart the cycle, so the character waits for another speeds[i] frames before moving again
+					timeCounters[i] = 0;					//Restart the cycle, so the character waits for another speeds[i] frames before moving again
 	
 					// Move the character down
 					++yPositions[i];
 
 					//Check if a character has reached the bottom
 					if (yPositions[i] >= 30) {
-						yPositions[i] = 30;						//Lock the character at the bottom
+						yPositions[i] = 30;				//Lock the character at the bottom
 						charIsAtBottom[i] = true;
 					}
 				}
@@ -107,16 +107,16 @@ void DigitalRain::DrawCharacters() {
 		//If all characters reached the bottom reset to top
 		if (allCharsAtBottom == numberOfChars) {
 			std::this_thread::sleep_for(std::chrono::milliseconds(100));
-			system("cls");															//Clear the screen
+			system("cls");								//Clear the screen
 
 			//Reset positions and characters for the next cycle
 			for (int i = 0; i < numberOfChars; ++i) {
 				yPositions[i] = 0;
 				characters[i] = static_cast<char>(33 + rand() % 10);
-				speeds[i] = 2 + rand() % 5;											//rand() generates a large random integer, % 5 (modulus operator) ensures the result is between 0 and 4
-				charIsAtBottom[i] = false;											//Reset the fallen flag
+				speeds[i] = 2 + rand() % 5;					//rand() generates a large random integer, % 5 (modulus operator) ensures the result is between 0 and 4
+				charIsAtBottom[i] = false;					//Reset the fallen flag
 			}
-			allCharsAtBottom = 0;   //Reset counter to start the process again
+			allCharsAtBottom = 0;                                                   //Reset counter to start the process again
 		}
 	}
 }
